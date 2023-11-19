fedd88a7a0851983339f11c1d7ddea28867819dc
druiz912 <druiz264@gmail.com>
2023-11-19 22:05:55 +0100

    refactor

--- main.js
+++ main.js
@@ -1,72 +1,83 @@
-// Agrega aquí los nombres de los archivos de audio correspondientes a cada animal
-const audioFiles = [
-  "cabra.mp3",
-  "gallo.mp3",
-  "pajaro.mp3",
-  "vaca.mp3",
-  "rana.mp3",
-  "pato.mp3",
-];
+// Asegúrate de que el DOM está completamente cargado antes de ejecutar el script
+document.addEventListener("DOMContentLoaded", function () {
+  const audioFiles = [
+    "cabra.mp3",
+    "gallo.mp3",
+    "pajaro.mp3",
+    "vaca.mp3",
+    "rana.mp3",
+    "pato.mp3",
+    "perro.mp3",
+    "gato.mp3",
+    "cerdo.mp3",
+  ];
 
-let currentAnimalIndex = 0;
-let isGameStarted = false;
-let timer;
+  const startBtn = document.getElementById("start-btn");
+  const timeBtn = document.getElementById("time-btn");
 
-function startGame() {
-  if (!isGameStarted) {
-    isGameStarted = true;
-    document.getElementById("start-btn").innerText = "Tiempo";
-    document.getElementById("time-btn").disabled = false;
-    loadNextAnimal();
-  } else {
-    clearTimeout(timer);
-    checkAnswer();
+  let isGameStarted = false;
+  let currentAnimalIndex = 0;
+  let timer;
+
+  function startGame() {
+    if (!isGameStarted) {
+      isGameStarted = true;
+      currentAnimalIndex = getRandomIndex();
+      startBtn.style.display = "none";
+      timeBtn.style.display = "block";
+      loadNextAnimal(currentAnimalIndex);
+    } else {
+      clearTimeout(timer);
+      checkAnswer();
+    }
   }
-}
 
-function loadNextAnimal() {
-  const imgElement = document.getElementById("animal-img");
-  imgElement.src = "./assets/incognit.jpg"; // Cambia a la imagen de pregunta
-  const audioElement = document.getElementById("animal-audio");
-  audioElement.src = audioFiles[currentAnimalIndex];
-  audioElement.load();
-  audioElement.play();
-  timer = setTimeout(() => {
-    checkAnswer();
-  }, audioElement.duration * 200); // Espera hasta que termine el audio
-}
-
-function playAudio() {
-  const audioElement = document.getElementById("animal-audio");
-  audioElement.currentTime = 0;
-  audioElement.play();
-}
-
-function checkAnswer() {
-  const imgElement = document.getElementById("animal-img");
-  const audioFileName = audioFiles[currentAnimalIndex];
-  const animalName = audioFileName.split(".")[0]; // Elimina la extensión del archivo
-  const imagePath = `./assets/animales/${animalName}.png`; // Ruta de la imagen en la carpeta de animales
-
-  imgElement.src = imagePath; // Cambia a la imagen correspondiente al animal
-
-  // Reinicia el juego si hay más animales, de lo contrario, muestra un mensaje de fin de juego
-  if (currentAnimalIndex < audioFiles.length - 1) {
-    currentAnimalIndex++;
-    setTimeout(() => {
-      loadNextAnimal();
-    }, 2000); // Espera 2 segundos antes de cargar el siguiente animal
-  } else {
-    alert("¡Juego terminado!");
-    resetGame();
+  function getRandomIndex() {
+    return Math.floor(Math.random() * audioFiles.length);
   }
-}
 
-function resetGame() {
-  isGameStarted = false;
-  currentAnimalIndex = 0;
-  document.getElementById("start-btn").innerText = "Iniciar juego";
-  document.getElementById("time-btn").disabled = true;
-  const imgElement = document.getElementById("animal-img");
-  imgElement.src = "./assets/game-audio-animal.jpg";
-}
+  function loadNextAnimal(currentAnimalIndex) {
+    const audioElement = document.getElementById("animal-audio");
+    const imgElement = document.getElementById("animal-img");
+
+    imgElement.src = "./assets/incognit.jpg"; // Cambia a la imagen de pregunta
+    const audioFileName = audioFiles[currentAnimalIndex];
+    audioElement.src = `./assets/audios/${audioFileName}`; // Ruta completa del archivo de audio
+    audioElement.load();
+    audioElement.play();
+    timer = setTimeout(() => {
+      checkAnswer(currentAnimalIndex);
+    }, audioElement.duration * 1000); // Espera hasta que termine el audio
+  }
+
+  function checkAnswer(currentAnimalIndex) {
+    const audioElement = document.getElementById("animal-audio");
+    const imgElement = document.getElementById("animal-img");
+
+    const audioFileName = audioFiles[currentAnimalIndex];
+    const animalName = audioFileName.split(".")[0]; // Elimina la extensión del archivo
+    const imagePath = `./assets/animales/${animalName}.png`; // Ruta de la imagen en la carpeta de animales
+
+    imgElement.src = imagePath; // Cambia a la imagen correspondiente al animal
+
+    // Reinicia el juego si hay más animales, de lo contrario, muestra un mensaje de fin de juego
+    if (currentAnimalIndex < audioFiles.length - 1) {
+      let newIndex = currentAnimalIndex++;
+      setTimeout(() => {
+        loadNextAnimal(newIndex);
+      }, 2000); // Espera 2 segundos antes de cargar el siguiente animal
+    } else {
+      alert("¡Juego terminado!");
+      resetGame();
+    }
+  }
+
+  function resetGame() {
+    isGameStarted = false;
+    currentAnimalIndex = 0;
+    startBtn.style.display = "block";
+    timeBtn.style.display = "none";
+    document.getElementById("animal-img").src =
+      "./assets/game-audio-animal.jpg";
+  }
+});
