3462ded4e2789107d47542d7f9d51cb65551e110
druiz912 <druiz264@gmail.com>
2024-03-09 15:37:09 +0100

    subida a public - refactor I

--- services/bingo-visual.js
+++ services/bingo-visual.js
@@ -1,37 +1,78 @@
 // bingo-visual.js
+let usedCards = []; // Para almacenar las cartas ya usadas
+let timerId; // Para almacenar el ID del temporizador
+let roundCount = 0; // Para contar las rondas
+let totalRounds; // Para almacenar el número total de rondas
+let isPaused = false; // Para controlar si el juego está en pausa
+
+let showContent;
+let stopTime;
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  let usedCards = []; // Para almacenar las cartas ya usadas
-  let timerId; // Para almacenar el ID del temporizador
-  let roundCount = 0; // Para contar las rondas
-  let totalRounds; // Para almacenar el número total de rondas
 
-  function generateImages() {
-    const imagesDiv = document.getElementById("images");
-    imagesDiv.innerHTML = ""; // Limpiar las imágenes anteriores
-
-    const roundTime = document.getElementById("roundTime").value;
-    const cardsNumber = document.getElementById("cardsNumber").value;
-
-    totalRounds = Math.ceil(63 / cardsNumber); // Calcular el número total de rondas
-    document.getElementById("roundsLeft").textContent = totalRounds - roundCount;
-
-    // Ocultar los divs una vez que el juego haya comenzado
+  
+  function hideContent() {
     document.getElementById("roundTimeDiv").style.display = "none";
     document.getElementById("cardsNumberDiv").style.display = "none";
-
-    for (let i = 0; i < cardsNumber; i++) {
-      let randomNumber;
-      do {
-        randomNumber = Math.floor(Math.random() * 63) + 1; // Generar un número aleatorio entre 1 y 63
-      } while (usedCards.includes(randomNumber));
-      usedCards.push(randomNumber);
-
-      const img = document.createElement("img");
-      img.src = `../assets/bingo-visual/card-${randomNumber}.png`; // Asumiendo que las imágenes son .png
-      imagesDiv.appendChild(img);
+    document.getElementById("input-container").style.display = "none";
+  }
+  
+  function updateRoundsLeft() {
+    document.getElementById("roundsLeft").textContent = totalRounds - roundCount;
+  }
+  
+  showContent = function() {
+    document.getElementById("content").style.display = "ruby-text";
+    document.getElementById("images").style.display = "flex";
+    document.getElementById("generateButton").style.display = "none"; // Ocultar el botón "Generar nuevas imágenes"
+  }
+  
+  stopTime = function() {
+    isPaused = true; // Pausar el juego
+    clearInterval(timerId); // Detiene el temporizador
+    document.getElementById("generateButton").textContent = "Reanudar ronda"; // Cambiar el texto del botón
+  }
+  
+  function restartGame() {
+    isPaused = false; // Reanudar el juego
+    usedCards = []; // Reiniciar las cartas usadas
+    roundCount = 0; // Reiniciar el contador de rondas
+    document.getElementById("images").innerHTML = ""; // Limpiar las imágenes
+    generateImages();
+    showContent();
+  }
+  
+  function generateImages() {
+    if (!isPaused) {
+      const imagesDiv = document.getElementById("images");
+      imagesDiv.innerHTML = ""; // Limpiar las imágenes anteriores
+  
+      const roundTime = document.getElementById("roundTime").value;
+      const cardsNumber = document.getElementById("cardsNumber").value;
+  
+      totalRounds = Math.ceil(63 / cardsNumber); // Calcular el número total de rondas
+      updateRoundsLeft();
+  
+      hideContent();
+  
+      for (let i = 0; i < cardsNumber; i++) {
+        let randomNumber;
+        do {
+          randomNumber = Math.floor(Math.random() * 63) + 1; // Generar un número aleatorio entre 1 y 63
+        } while (usedCards.includes(randomNumber));
+        usedCards.push(randomNumber);
+  
+        const img = document.createElement("img");
+        img.src = `../assets/bingo-visual/card-${randomNumber}.png`; // Asumiendo que las imágenes son .png
+        imagesDiv.appendChild(img);
+      }
+  
+      startTimer(roundTime);
+      clearImagesAfterTime(roundTime, imagesDiv);
     }
-
-    // Iniciar el cronómetro
+  }
+  
+  function startTimer(roundTime) {
     let timeLeft = roundTime;
     document.getElementById("timeLeft").textContent = timeLeft;
     timerId = setInterval(() => {
@@ -41,8 +82,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         clearInterval(timerId);
       }
     }, 1000);
-
-    // Limpiar las imágenes después del tiempo de la ronda
+  }
+  
+  function clearImagesAfterTime(roundTime, imagesDiv) {
     setTimeout(() => {
       imagesDiv.innerHTML = "";
       clearInterval(timerId);
@@ -51,15 +93,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
         roundCount = 0; // Reiniciar el contador de rondas
       }
       roundCount++;
-      document.getElementById("roundsLeft").textContent =
-        totalRounds - roundCount;
+      updateRoundsLeft();
       if (roundCount < totalRounds) {
         generateImages(); // Generar nuevas imágenes si aún no se han completado todas las rondas
       }
     }, roundTime * 1000);
   }
-
   document
     .getElementById("generateButton")
-    .addEventListener("click", generateImages);
+    .addEventListener("click", restartGame);
+
+  document.getElementById("stopTimeButton").addEventListener("click", stopTime);
 });
